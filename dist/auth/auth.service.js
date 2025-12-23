465291fcd330157651df26ea964b2b8a57c8a2c4
Hector Barazorda <ph.barazorda@gmail.com>
2025-12-23 07:47:28 -0500

    feat(auth): change login method to remove async keyword

--- dist/auth/auth.service.js
+++ dist/auth/auth.service.js
@@ -62,7 +62,7 @@ let AuthService = class AuthService {
         }
         return null;
     }
-    async login(user) {
+    login(user) {
         const payload = { email: user.email, sub: user.id, role: user.role };
         return {
             access_token: this.jwtService.sign(payload),
